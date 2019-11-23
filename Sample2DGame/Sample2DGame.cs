5f0f59d72eb07c9147dec30b5599fddd22e81fd2
Alex Sherman <asherman1024@gmail.com>
2019-11-22 21:14:41 -0600

    Initial commit

---
+++ Sample2DGame/Sample2DGame.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Spectrum;
+using Spectrum.Framework.Entities;
+using Spectrum.Framework.Graphics;
+using Spectrum.Framework.Screens;
+using Spectrum.Framework.Screens.InputElements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample2DGame
+{
+    public class Sample2DGame : SpectrumGame
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+            var camera = new Camera2D();
+            var layout = Root.AddElement(new LinearLayout());
+            layout.BackgroundColor = "0xffffff";
+            layout.AddElement(new TextElement("Hello Thadius"));
+            layout.Height = 1.0;
+            layout.Width = 1.0;
+            var scene = layout.AddElement(new SceneScreen(camera));
+            camera.Position = new Vector2(0, 0);
+            var manager = scene.Manager;
+            manager.CreateEntity(new InitData<GameObject2D>().Call("AddComponent", "Thadius"));
+        }
+    }
+}
---
+++ Sample2DGame/Thadius.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Spectrum.Framework;
+using Spectrum.Framework.Entities;
+using Spectrum.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample2DGame
+{
+    [LoadableType]
+    public class Thadius : Component<GameObject2D>, IUpdate
+    {
+        public override void Initialize(Entity e)
+        {
+            base.Initialize(e);
+            P.Texture = "thadius";
+            P.Bounds = P.Texture.Texture.Bounds;
+            // Center the sprite on Thadius's position
+            P.Bounds.X = -P.Bounds.Width / 2;
+            P.Bounds.Y = -P.Bounds.Height / 2;
+        }
+
+        public void Update(float dt)
+        {
+            var multiplier = InputState.Current.IsKeyDown(Keys.LeftShift) ? 2 : 1;
+            if (InputState.Current.IsKeyDown(Keys.Q))
+                P.Rotation += dt * multiplier;
+            if (InputState.Current.IsKeyDown(Keys.E))
+                P.Rotation -= dt * multiplier;
+            if (InputState.Current.IsKeyDown(Keys.W))
+                P.Position += Vector2.Transform(Vector2.UnitY * dt * 500 * multiplier, Matrix.CreateRotationZ(P.Rotation));
+        }
+    }
+}
